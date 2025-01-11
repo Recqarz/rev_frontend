b4f8bd848975b7dd81e89cdd5bcf8c1f382f767c
ranjan-recqarz <ranjan.yadav@recqarz.com>
2025-01-11 13:43:55 +0530

    login api has been integrated in redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useSelector } from "react-redux";
 import AllRoutes from "./components/AllRoutes";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
@@ -8,19 +7,16 @@ import VerifyOTP from "./pages/resetpasword/VerifyOTP";
 import PageNotFound from "./pages/PageNotFound";
 
 function App() {
-  const userReducer = useSelector((store) => store.userReducer);
-  const caseReducer = useSelector((store) => store.caseReducer);
-  console.log(caseReducer, userReducer);
   return (
     <Routes>
-    {/* <Route path="/" element={<Home />} /> */}
-    <Route path="/" element={<Login />} />
-    <Route path="/resetpassword" element={<ResetDashboard/>} />
-    <Route path="/register" element={<Register/>} />
-    <Route path="/verifyotp" element={<VerifyOTP/>} />
-    <Route path="/*" element={<AllRoutes/>} />
-    <Route path="*" element={<PageNotFound />} />
-  </Routes>
+      {/* <Route path="/" element={<Home />} /> */}
+      <Route path="/" element={<Login />} />
+      <Route path="/resetpassword" element={<ResetDashboard />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/verifyotp" element={<VerifyOTP />} />
+      <Route path="/*" element={<AllRoutes />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
   );
 }
 

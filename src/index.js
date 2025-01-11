38ecde4f200eb33eba3e3c6e42fa3fb888acb715
ranjan-recqarz <ranjan.yadav@recqarz.com>
2025-01-11 10:37:02 +0530

    react-toastify has been implemented

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import ReactToastify from "./utils/react-toastify/ReactToastiry";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
+      <ReactToastify />
       <App />
     </BrowserRouter>
   </Provider>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import { toastSuccess } from "../utils/react-toastify/ReactToastiry";
+
 
 const Login = () => {
+  const handleToast = () => {
+    toastSuccess("toast success!");
+  };
 
-  return <div>Login</div>;
+  return (
+    <div>
+      <h1>Login Page.</h1>
+      <button onClick={handleToast}>login</button>
+    </div>
+  );
 };
 
 export default Login;

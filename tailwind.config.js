b9c3d747c27f441646519fa56ef29ab6c99f87be
ranjan-recqarz <ranjan.yadav@recqarz.com>
2025-01-14 10:47:55 +0530

    colors config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#0000FF', // Blue
-        secondary: '#ADD8E6', // Light Blue
-        accent: '#808080', // Gray
+        /** */
+        primary_dark: "#073c4e",
+        primary_light: "#51677E",
+
+        /** */
+        secondary_dark: "#b97f18",
+        secondary_light: "#f1ce91",
+
+        /** */
+        accent_dark: "#ff0000",
+        accent_light: "#ff8989",
       },
     },
   },
   plugins: [],
-}
+};

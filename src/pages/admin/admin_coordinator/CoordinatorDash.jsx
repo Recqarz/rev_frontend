84f0f2936fd58db8b87da71af5602b6ee38c114a
sumansauravnov <suman.saurav@recqarz.com>
2025-01-14 18:46:53 +0530

    fetch the user data

--- src/pages/admin/admin_coordinator/CoordinatorDash.jsx
+++ src/pages/admin/admin_coordinator/CoordinatorDash.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../../components/Navbar'
 import Table from '../../../components/Table'
 import AddCoordinator from '../../../components/AddCoordinator'
+import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllUserData } from '../../../redux/users/userAction'
 
 const CoordinatorDash = ({ isOpen, toggleSidebar }) => {
+  const dispatch=useDispatch();
+  const allUser=useSelector((user)=>user.allUserReducer.data);
+  console.log("user", allUser)
+
+
+useEffect(()=>{
+dispatch(getAllUserData());
+},[])
+
+
   return (
     <div className="w-full ">
       <Navbar isOpen={isOpen} toggleSidebar={toggleSidebar} />
+     
       <Table />
-      <AddCoordinator />
+      {/* <AddCoordinator /> */}
     </div>
   )
 }

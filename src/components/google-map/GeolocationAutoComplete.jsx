1c60af08422ffefbc287c7387cb55e8cffb92f98
ranjan-recqarz <ranjan.yadav@recqarz.com>
2025-01-29 19:25:43 +0530

    updated GeolocationAutoComplete.jsx

--- src/components/google-map/GeolocationAutoComplete.jsx
+++ src/components/google-map/GeolocationAutoComplete.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import useGoogleMaps from "../../utils/customHooks/useGoogleMaps";
 
-const LocationAutocomplete = ({ onSelect }) => {
+const GeolocationAutoComplete = ({ onSelect }) => {
   const isGoogleMapsLoaded = useGoogleMaps(); // Load Google Maps dynamically
   const [inputValue, setInputValue] = useState("");
 
@@ -19,8 +19,10 @@ const LocationAutocomplete = ({ onSelect }) => {
       if (selectedPlace.geometry) {
         const locationData = {
           address: selectedPlace.formatted_address,
-          lat: selectedPlace.geometry.location.lat(),
-          lng: selectedPlace.geometry.location.lng(),
+          coordinates: [
+            selectedPlace.geometry.location.lng(),
+            selectedPlace.geometry.location.lat(),
+          ],
         };
         onSelect(locationData);
       }
@@ -28,15 +30,20 @@ const LocationAutocomplete = ({ onSelect }) => {
   }, [isGoogleMapsLoaded, onSelect]);
 
   return (
-    <input
-      id="autocomplete"
-      type="text"
-      value={inputValue}
-      onChange={(e) => setInputValue(e.target.value)}
-      placeholder="Enter a location"
-      className="w-full p-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-    />
+    <div>
+      <label className="block text-sm font-medium text-gray-700 mb-1">
+        geocode location
+      </label>
+      <input
+        id="autocomplete"
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Enter a location"
+        className="w-full p-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+    </div>
   );
 };
 
-export default LocationAutocomplete;
+export default GeolocationAutoComplete;
